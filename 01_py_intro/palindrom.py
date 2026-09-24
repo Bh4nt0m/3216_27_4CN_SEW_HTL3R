fa1153f79786023a024b99115b8059a5ce3ab4db
Clemens Zimmer <3216@htl.rennweg.at>
2026-09-24 17:21:56 +0200

    Erstellung von palindrom_product und get_dec_hex_palindrom

--- 01_py_intro/palindrom.py
+++ 01_py_intro/palindrom.py
@@ -1,5 +1,5 @@
 """
-
+# UE 1 - 24.09.26 F
 """
 
 import re
@@ -9,7 +9,8 @@ __example__ = "SEW4/01/F"
 __date__ = "24.09.2026"
 __license__ = "GNU GPLv3"
 
-def is_palindrom(s:str):
+
+def is_palindrom(s: str):
     """
     >>> is_palindrom("anna")
     True
@@ -28,7 +29,8 @@ def is_palindrom(s:str):
     """
     return s == s[::-1]
 
-def is_palindrom_sentence(s:str):
+
+def is_palindrom_sentence(s: str):
     """
     >>> is_palindrom_sentence("Was it a car or a cat I saw")
     True
@@ -45,20 +47,75 @@ def is_palindrom_sentence(s:str):
     >>> is_palindrom_sentence(3.14)
     Traceback (most recent call last):
      ...
-    AttributeError: 'float' object has no attribute 'lower'
+    TypeError: expected string or bytes-like object, got 'float'
 
     """
 
-    s = s.lower().replace(" ", "").replace(".", "").replace(",", "").replace("-", "").replace("!", "").replace("?", "")
+    s = re.sub(r"[\s\W_]+", "", s).lower()
     return s == s[::-1]
+
+
+def palindrom_product(x) -> int:
+    """
+    >>> palindrom_product(1000000)
+    906609
+    >>> palindrom_product(906609)
+    888888
+    >>> palindrom_product(100000)
+    99999
+    >>> palindrom_product(100)
+    -1
+    >>> palindrom_product(0)
+    -1
+
+    """
+
+    max_value = -1
+    for i in range(100, 1000):
+        for e in range(100, 1000):
+            current = i * e
+            if current >= x:
+                break
+            if is_palindrom(str(current)):
+                if current > max_value:
+                    max_value = current
+    return max_value
+
+
+def get_dec_hex_palindrom(x):
+    """
+    >>> get_dec_hex_palindrom(1000)
+    979
+    >>> get_dec_hex_palindrom(900)
+    787
+    >>> get_dec_hex_palindrom(700)
+    626
+    """
+    set_saved = set()
+    i, e = 0, 0
+    max_value = -1
+    while i <= x:
+        i+=1
+        while e <= x:
+            e+=1
+            current = i * e
+            if set_saved.__contains__(current):
+                continue
+            set_saved.add(current)
+            if current >= x:
+                break
+            if is_palindrom(str(current)) and is_palindrom(str(hex(current))[2:]):
+                if current > max_value:
+                    max_value = current
+        e = 0
+    return max_value
+
 
 
 def main():
     pass
 
+
 if __name__ == "__main__":
+    get_dec_hex_palindrom(845548)
     main()
-
-
-
-
