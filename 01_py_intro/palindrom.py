b45de9fb65710fb4773672d0812e8a685392db5f
Clemens Zimmer <3216@htl.rennweg.at>
2026-09-24 15:05:33 +0200

    Erstellung von default-files und palindrom.py

---
+++ 01_py_intro/palindrom.py
@@ -0,0 +1,14 @@
+"""
+
+"""
+
+__author__ = "Clemens Zimmer"
+__example__ = "SEW4/01/F" #Gegenstand/Übungsblatt/Aufgabe(Kapitel)
+__date__ = "24.09.2026"
+__license__ = "GNU GPLv3"
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ Test.py
@@ -0,0 +1,14 @@
+"""
+
+"""
+
+__author__ = "Clemens Zimmer"
+__example__ = "SEW4/01/B" #Gegenstand/Übungsblatt/Aufgabe(Kapitel)
+__date__ = "01.09.2026"
+__license__ = "GNU GPLv3"
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
